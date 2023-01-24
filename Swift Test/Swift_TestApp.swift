1a6eceb07e89d01104aa5a75d2b4932f012f6f3a
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-01-24 18:48:44 -0500

    Initial Commit

---
+++ Swift Test/Swift_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TestApp.swift
+//  Swift Test
+//
+//  Created by Zhengtian Li on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
